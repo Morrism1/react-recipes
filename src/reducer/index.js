5d84394eddc470906b2a6acd327fa59b62e47854
Morrism1 <morrismontana0@gmail.com>
2021-05-15 00:06:22 +0200

    Combine reducers

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,30 +1,7 @@
-/* eslint-disable no-param-reassign */
-// @ts-nocheck
-import produce from 'immer';
-import { LOAD_CATEGORIES_BEGIN, LOAD_CATEGORIES_SUCCESS, LOAD_CATEGORIES_ERROR } from '../actions/index';
+import { combineReducers } from 'redux';
+import categories from './categories';
+import meals from './meals';
 
-const initialState = {
-  categories: [],
-  loading: false,
-  error: null,
-};
-
-const reducer = produce((draft, action) => {
-  switch (action.type) {
-    case LOAD_CATEGORIES_BEGIN:
-      draft.loading = true;
-      draft.error = null;
-      return;
-    case LOAD_CATEGORIES_SUCCESS:
-      draft.loading = false;
-      draft.categories = action.payload;
-      return;
-    case LOAD_CATEGORIES_ERROR:
-      draft.error = action.error;
-      break;
-    default:
-      break;
-  }
-}, initialState);
-
-export default reducer;
+export default combineReducers({
+  categories, meals,
+});
