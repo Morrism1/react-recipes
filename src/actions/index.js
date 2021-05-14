bd5ffa66fc1f06dbf7c7144bc72fe78f38dd54ac
Morrism1 <morrismontana0@gmail.com>
2021-05-15 00:11:18 +0200

    Create action to handle the getting Meals from the api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,12 @@
-import getCategories from '../api';
+import { getCategories, getMeals } from '../api';
 
 export const LOAD_CATEGORIES_BEGIN = 'LOAD_CATEGORIES_BEGIN';
 export const LOAD_CATEGORIES_SUCCESS = 'LOAD_CATEGORIES_SUCCESS';
 export const LOAD_CATEGORIES_ERROR = 'LOAD_CATEGORIES_ERROR';
+
+export const LOAD_MEALS_BEGIN = 'LOAD_MEALS_BEGIN';
+export const LOAD_MEALS_SUCCESS = 'LOAD_MEALS_SUCCESS';
+export const LOAD_MEALS_ERROR = 'LOAD_CATEGORIES_ERROR';
 
 export const loadCategories = () => (dispatch) => {
   dispatch({ type: LOAD_CATEGORIES_BEGIN });
@@ -12,5 +16,16 @@ export const loadCategories = () => (dispatch) => {
     })
     .catch((error) => {
       dispatch({ type: LOAD_CATEGORIES_ERROR, error });
+    });
+};
+
+export const loadMeals = (strCategory) => (dispatch) => {
+  dispatch({ type: LOAD_MEALS_BEGIN });
+  getMeals(strCategory)
+    .then((meals) => {
+      dispatch({ type: LOAD_MEALS_SUCCESS, payload: meals });
+    })
+    .catch((error) => {
+      dispatch({ type: LOAD_MEALS_ERROR, error });
     });
 };
