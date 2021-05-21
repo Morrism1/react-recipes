0b778b8d21ff7c5c22f95b49f7176a3fd8a91b20
Morrism1 <morrismontana0@gmail.com>
2021-05-21 02:42:42 +0200

    Add filter action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { getCategories, getMeals } from '../api';
+import { getCategories, getMeals, getMealData } from '../api';
 
 export const LOAD_CATEGORIES_BEGIN = 'LOAD_CATEGORIES_BEGIN';
 export const LOAD_CATEGORIES_SUCCESS = 'LOAD_CATEGORIES_SUCCESS';
@@ -6,7 +6,13 @@ export const LOAD_CATEGORIES_ERROR = 'LOAD_CATEGORIES_ERROR';
 
 export const LOAD_MEALS_BEGIN = 'LOAD_MEALS_BEGIN';
 export const LOAD_MEALS_SUCCESS = 'LOAD_MEALS_SUCCESS';
-export const LOAD_MEALS_ERROR = 'LOAD_CATEGORIES_ERROR';
+export const LOAD_MEALS_ERROR = 'LOAD_MEALS_ERROR';
+
+export const LOAD_MEALDATA_BEGIN = 'LOAD_MEALDATA_BEGIN';
+export const LOAD_MEALDATA_SUCCESS = 'LOAD_MEALDATA_SUCCESS';
+export const LOAD_MEALDATA_ERROR = 'LOAD_MEALDATA_ERROR';
+
+export const CHANGE_FILTER = 'CHANGE_FILTER';
 
 export const loadCategories = () => (dispatch) => {
   dispatch({ type: LOAD_CATEGORIES_BEGIN });
@@ -29,3 +35,19 @@ export const loadMeals = (strCategory) => (dispatch) => {
       dispatch({ type: LOAD_MEALS_ERROR, error });
     });
 };
+
+export const loadMealDetails = (idMeal) => (dispatch) => {
+  dispatch({ type: LOAD_MEALDATA_BEGIN });
+  getMealData(idMeal)
+    .then((mealData) => {
+      dispatch({ type: LOAD_MEALDATA_SUCCESS, payload: mealData });
+    })
+    .catch((error) => {
+      dispatch({ type: LOAD_MEALDATA_ERROR, error });
+    });
+};
+
+export const filter = (filter) => ({
+  type: CHANGE_FILTER,
+  payload: filter,
+});
