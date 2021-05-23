44c5df429a3317fa4b2222417a752f70020b42b7
Morrism1 <morrismontana0@gmail.com>
2021-05-24 00:50:53 +0200

    Create category Meal component

--- src/components/CategoryMeals.js
+++ src/components/CategoryMeals.js
@@ -1,19 +1,26 @@
+/* eslint-disable no-console */
 // @ts-nocheck
 import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Card, Text, Image } from '@mantine/core';
+import { Link } from '@reach/router';
 import Loading from './Loading';
 import NotFoundPage from './NotFound';
-import getMealsByCategory from '../selectors';
-import { loadMeals } from '../actions/index';
+import { getMealsByCategory } from '../selectors';
+import { loadMeals, filter } from '../actions/index';
+import './CategoryMeals.css';
 
 function CategoryMeals({
-  category, loading, loadMeals, meals,
+  category, loading, loadMeals, meals, filterValue,
 }) {
   useEffect(() => {
-    loadMeals(category.strCategory);
-  }, [category]);
+    if (filterValue === 'Current') {
+      loadMeals(category.strCategory);
+    } else {
+      loadMeals(filterValue);
+    }
+  }, [filterValue]);
 
   if (loading) {
     return <Loading />;
@@ -24,21 +31,23 @@ function CategoryMeals({
   }
 
   return (
-    <div>
+    <div className="mt-4">
       <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {meals.map((meal) => (
           <li key={meal.idMeal}>
-            <Card>
-              <Image
-                src={meal.strMealThumb}
-                height={160}
-                alt="Noway"
-              />
+            <Link to={`/meals/${meal.idMeal}`}>
+              <Card className="card--shadow">
+                <Image
+                  src={meal.strMealThumb}
+                  height={160}
+                  alt="Noway"
+                />
 
-              <Text weight={500} size="lg">
-                {meal.strMeal}
-              </Text>
-            </Card>
+                <Text weight={500} size="lg">
+                  {meal.strMeal}
+                </Text>
+              </Card>
+            </Link>
           </li>
         ))}
       </ul>
@@ -57,6 +66,7 @@ CategoryMeals.propTypes = {
   category: PropTypes.objectOf(PropTypes.string),
   loading: PropTypes.bool.isRequired,
   loadMeals: PropTypes.func.isRequired,
+  filterValue: PropTypes.string.isRequired,
   meals: PropTypes.arrayOf(PropTypes.object),
 };
 
@@ -64,6 +74,7 @@ const mapToState = (state, ownProps) => ({
   category: getMealsByCategory(state, ownProps),
   meals: state.meals.meals.meals,
   loading: state.meals.loading,
+  filterValue: state.filter.filter,
 });
 
-export default connect(mapToState, { loadMeals })(CategoryMeals);
+export default connect(mapToState, { loadMeals, filter })(CategoryMeals);
