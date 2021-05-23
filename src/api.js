b809aae911433bf39e57321bb578095f5b6daba6
Morrism1 <morrismontana0@gmail.com>
2021-05-24 00:52:05 +0200

    Refactor urls in the api functions

--- src/api.js
+++ src/api.js
@@ -1,19 +1,21 @@
 import axios from 'axios';
 
+const PREFIX = 'https://www.themealdb.com/api/json/v1/1';
+
 const getCategories = async () => {
-  const data = await axios.get('/categories.php').then((res) => res);
+  const data = await axios.get(`${PREFIX}/categories.php`).then((res) => res);
 
   return data.data;
 };
 
 const getMeals = async (categoryName) => {
-  const data = await axios.get(`/filter.php?c=${categoryName}`).then((res) => res);
+  const data = await axios.get(`${PREFIX}/filter.php?c=${categoryName}`).then((res) => res);
 
   return data.data;
 };
 
 const getMealData = async (idMeal) => {
-  const data = await axios.get(`/lookup.php?i=${idMeal}`).then((res) => res);
+  const data = await axios.get(`${PREFIX}/lookup.php?i=${idMeal}`).then((res) => res);
 
   return data.data;
 };
