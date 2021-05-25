5634b685b74535c0e9746cbcc8cc4cce16c11f6c
Morrism1 <morrismontana0@gmail.com>
2021-05-25 21:18:03 +0200

    Fix jss console warning

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,17 @@
 // @ts-nocheck
 import React from 'react';
-import { MantineProvider, Container, theming } from '@mantine/core';
+import { MantineProvider, Container } from '@mantine/core';
 import { Router, Redirect, LocationProvider } from '@reach/router';
-import { createUseStyles } from 'react-jss';
 import Categories from './Categories';
 import './Categories.css';
 import CategoryMeals from './CategoryMeals';
 import MealData from './MealData';
 import Navbar from './Navbar';
 
-const useStyles = createUseStyles(
-  () => ({
-    '@global': {
-      body: {
-        backgroundColor: '#fefefe',
-      },
-    },
-  }),
-  { theming },
-);
 function App() {
-  useStyles();
   return (
-    <MantineProvider
-      theme={{
-        fontFamily: 'Inter, sans-serif',
-        lineHeight: 1.2,
-      }}
-    >
-      <Container size={1200} padding={0}>
+    <MantineProvider>
+      <Container size={1200} padding={12}>
         <LocationProvider>
           <Navbar />
         </LocationProvider>
