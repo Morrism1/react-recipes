5eba55f51160d690ce2d9a8811383fe32985a857
Morrism1 <morrismontana0@gmail.com>
2021-05-25 02:27:07 +0200

    Add tests for the app component

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,15 @@
+// @ts-nocheck
+import React from 'react';
+import { cleanup } from '@testing-library/react';
+import { render } from '../../test-utils';
+import '@testing-library/jest-dom/extend-expect';
+import App from '../App';
+
+afterEach(cleanup);
+
+describe('Component', () => {
+  test('should render Categories', () => {
+    render(<App />, { initialState: { filter: 'Chicken' } });
+    expect(<App />).toMatchSnapshot();
+  });
+});
