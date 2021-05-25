d8e55d98a65bb06feecd9404c0767440d512f176
Morrism1 <morrismontana0@gmail.com>
2021-05-25 02:29:54 +0200

    Add actions tests

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -2,6 +2,11 @@ import {
   filter, loadCategories, loadMeals, loadMealDetails,
 } from './index';
 
+const mockFetch = (data) => jest.fn().mockImplementation(() => Promise.resolve({
+  ok: true,
+  json: () => data,
+}));
+
 test('filter', () => {
   const value = filter();
 
@@ -9,8 +14,32 @@ test('filter', () => {
 });
 
 test('Load categories', () => {
+  const mockDispatch = jest.fn;
   const result = loadCategories();
   expect(result).toEqual(expect.any(Function));
+  loadCategories()(mockDispatch);
+  expect(mockDispatch.call.length).toBe(1);
+});
+
+describe('load Categories', () => {
+  it('fetches all categories', async () => {
+    global.fetch = mockFetch({});
+    const dispatch = jest.fn();
+    await loadCategories()(dispatch);
+    expect(dispatch.mock.calls.length).toBe(2);
+    expect(dispatch.mock.calls[0][0]).toEqual({ type: 'LOAD_CATEGORIES_BEGIN' });
+    expect(dispatch.mock.calls[1][0]).toEqual({ type: 'LOAD_CATEGORIES_SUCCESS', payload: expect.any(Object) });
+  });
+
+  it('handles error', async () => {
+    const error = { message: 'Not Found' };
+    global.fetch = () => Promise.reject(error);
+    const dispatch = jest.fn();
+    await loadCategories()(dispatch);
+    expect(dispatch.mock.calls.length).toBe(2);
+    expect(dispatch.mock.calls[0][0]).toEqual({ type: 'LOAD_CATEGORIES_BEGIN' });
+    expect(dispatch.mock.calls[1][0]).toEqual({ type: 'LOAD_CATEGORIES_SUCCESS', payload: expect.any(Object) });
+  });
 });
 
 test('Load meals', () => {
